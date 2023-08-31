752f6a7047aff6955ec40244cf4514d11546ce25
seungjun-dev <seungjun91@naver.com>
2023-08-31 22:18:40 +0900

    Swems Web

--- js/swems.js
+++ js/swems.js
@@ -1,45 +1,106 @@
-import Chart from '@toast-ui/chart'
-import '@toast-ui/chart/dist/toastui-chart.min.css';
-
-const el = document.getElementById('chart');
-const data = {
-    categories: ['Sweet', 'Warm', 'Enthusiastic', 'Modern', 'Sorrow'],
-    series: [
-        {
-            name: '위잉위잉',
-            data: [1.40, 3.08, 4.60, 4.57, 3.02],
-        },
-        {
-            name: 'Gee',
-            data: [6.13, 4.67, 5.53, 3.89, 1.00],
-        },
-        {
-            name: '롤리폴리',
-            data: [3.60, 3.61, 5.49, 2.84, 1.11],
-        },
-        {
-            name: '봄이좋냐',
-            data: [5.13, 5.75, 3.78, 3.89, 2.02],
-        },
-    ],
-};
-
-const options = {
-    chart: { width: 700, height: 400 },
-    series: {
-        showArea: true
-    }
-}
-
-const chart = Chart.radarChart({ el, data, options });
-//const chart = new RadarChart({ el, data, options });
-
 $.ajax({
-    url: 'data_utf8.csv',
-    dataType: 'text',
-}).done(successFunction);
+    url: "../data/swems.json",
+    dataType: "json",
+}).done(function (res) {
+    const grid = new tui.Grid({
+        el: document.getElementById("grid"),
+        width: "50%",
+        data: res,
+        pageOptions: {
+            useClient: true,
+            perPage: 15,
+        },
+        scrollX: false,
+        scrollY: false,
+        //ID,TITLE,ARTIST,SWEET,WARM,ENTHUSIASTIC,MODERN,SORROW,SCORE
+        columns: [
+            {
+                header: "ID",
+                name: "id",
+                width: "auto",
+            },
+            {
+                header: "TITLE",
+                name: "title",
+                width: "auto",
+            },
+            {
+                header: "ARTIST",
+                name: "artist",
+                width: "auto",
+            },
+            {
+                header: "SWEET",
+                name: "sweet",
+                width: "auto",
+            },
+            {
+                header: "WARM",
+                name: "warm",
+                width: "auto",
+            },
+            {
+                header: "ENTHUSIASTIC",
+                name: "enthusiastic",
+                width: "auto",
+            },
+            {
+                header: "MODERN",
+                name: "modern",
+                width: "auto",
+            },
+            {
+                header: "SORROW",
+                name: "sorrow",
+                width: "auto",
+            },
+        ],
+    });
 
-function successFunction(res) {
-    songList = decodeURI(res);
-    console.log(songList);
-}
+    tui.Grid.applyTheme("striped");
+
+    grid.on("click", getSwemsData);
+
+    function getSwemsData(res) {
+        const rowData = grid.getRowAt(res.rowKey);
+        console.log(rowData);
+
+        $("#chart").empty();
+
+        drawChart(rowData);
+    }
+
+    function drawChart(rowData) {
+        const Chart = toastui.Chart;
+
+        const el = document.getElementById("chart");
+        const data = {
+            categories: ["Sweet", "Warm", "Enthusiastic", "Modern", "Sorrow"],
+            series: [
+                {
+                    name: rowData.title,
+                    data: [
+                        rowData.sweet,
+                        rowData.warm,
+                        rowData.enthusiastic,
+                        rowData.modern,
+                        rowData.sorrow,
+                    ],
+                },
+            ],
+        };
+
+        const options = {
+            chart: {
+                title: rowData.title,
+                width: 700,
+                height: 400,
+            },
+            series: {
+                showArea: true,
+            },
+        };
+
+        const chart = Chart.lineChart({ el, data, options });
+    }
+});
